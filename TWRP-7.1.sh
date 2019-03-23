1a39d7815e345796494774d8796fed2b2e3c45d8
JarlPenguin <jarlpenguin@outlook.com>
2019-03-23 16:53:23 +0300

    Add make clean && make clobber

--- TWRP-7.1.sh
+++ TWRP-7.1.sh
@@ -2,12 +2,13 @@
 echo -e "\e[36mScript to automate TWRP (7.1) Recovery builds for Moto G5S (montana)"
 echo -e "Initializing...\e[0m"
 PATH=~/bin:$PATH
-cd <directory>
+cd ~/Documents/TWRP
 echo -e "\e[36mSyncing from source...\e[0m"
 repo sync -c -f --force-sync --no-tag --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 echo -e "\e[36mBuilding...\e[0m"
-export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_montana-eng
+make clean && make clobber
 make recoveryimage
 echo -e "\e[36mBuild process ended! Check the terminal for any errors.\e[0m"
