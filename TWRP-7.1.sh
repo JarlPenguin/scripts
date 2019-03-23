67086c888c7ee18aa4fe7bbbdfd176fc8e26e92b
JarlPenguin <jarlpenguin@outlook.com>
2019-03-23 19:51:58 +0300

    Update TWRP scripts to address dtbtool error

--- TWRP-7.1.sh
+++ TWRP-7.1.sh
@@ -2,13 +2,19 @@
 echo -e "\e[36mScript to automate TWRP (7.1) Recovery builds for Moto G5S (montana)"
 echo -e "Initializing...\e[0m"
 PATH=~/bin:$PATH
-cd ~/Documents/TWRP
+cd <directory of TWRP repo>
 echo -e "\e[36mSyncing from source...\e[0m"
+# Optimized sync
 repo sync -c -f --force-sync --no-tag --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 echo -e "\e[36mBuilding...\e[0m"
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_montana-eng
 make clean && make clobber
+mkdir <directory of TWRP repo>/out
+mkdir <directory of TWRP repo>/out/host
+mkdir <directory of TWRP repo>/out/host/linux-x86
+mkdir <directory of TWRP repo>/out/host/linux-x86/bin
+cp <directory of TWRP repo>/device/motorola/montana/dtbtool/dtbtool_moto <directory of TWRP repo>/out/host/linux-x86/bin/dtbtool_moto
 make recoveryimage
 echo -e "\e[36mBuild process ended! Check the terminal for any errors.\e[0m"
--- TWRP-9.0.sh
+++ TWRP-9.0.sh
@@ -2,7 +2,7 @@
 echo -e "\e[36mScript to automate TWRP (9.0) Recovery builds for Moto G5S (montana)"
 echo -e "Initializing...\e[0m"
 PATH=~/bin:$PATH
-cd ~/Documents/TWRP-9.0
+cd <directory of TWRP 9.0 repo>-9.0
 echo -e "\e[36mSyncing from source...\e[0m"
 repo sync -c -f --force-sync --no-tag --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 echo -e "\e[36mBuilding...\e[0m"
@@ -10,5 +10,10 @@ echo -e "\e[36mBuilding...\e[0m"
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_montana-eng
 make clean && make clobber
+mkdir <directory of TWRP 9.0 repo>-9.0/out
+mkdir <directory of TWRP 9.0 repo>-9.0/out/host
+mkdir <directory of TWRP 9.0 repo>-9.0/out/host/linux-x86
+mkdir <directory of TWRP 9.0 repo>-9.0/out/host/linux-x86/bin
+cp <directory of TWRP 9.0 repo>-9.0/device/motorola/montana/dtbtool/dtbtool_moto <directory of TWRP 9.0 repo>-9.0/out/host/linux-x86/bin/dtbtool_moto
 make recoveryimage
 echo -e "\e[36mBuild process ended! Check the terminal for any errors.\e[0m"
