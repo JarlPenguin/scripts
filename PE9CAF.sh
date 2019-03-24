91c78fbb91aea081c1273fd0769c49943db33ea9
JarlPenguin <jarlpenguin@outlook.com>
2019-03-25 01:42:15 +0300

    replace with placeholders

--- PE9CAF.sh
+++ PE9CAF.sh
@@ -2,7 +2,7 @@
 echo -e "\e[36mScript to automate Pixel Experience 9.0 (CAF) builds for Moto G5S (montana)"
 echo -e "Initializing...\e[0m"
 PATH=~/bin:$PATH
-cd ~/Documents/PixelExperience9CAF
+cd <directory>
 echo -e "\e[36mSyncing from source...\e[0m"
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 echo -e "\e[36mBuilding...\e[0m"
